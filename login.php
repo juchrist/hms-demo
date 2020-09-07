7a15af9f0acbb64d9d2a048b6b00f5de2bb809dd
juchrist <kingjuwon4christ@gmail.com>
2020-09-07 11:39:17 +0100

    Initial Commit

---
+++ login.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>HMS | Login 2</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+
+    <div class="loginColumns animated fadeInDown">
+        <div class="row">
+
+            <div class="col-md-6">
+                <h2 class="font-bold">Welcome to HMS</h2>
+
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+                    <hr/>
+                <p>
+                    Quick Links
+                </p>
+
+                <p>
+                    <a href="">Link 1</a>
+                </p>
+                <p>
+                        <a href="">Link 2</a>
+                    </p>
+                    <p>
+                            <a href="">Link 3</a>
+                        </p>
+                                        
+            </div>
+            <div class="col-md-6">
+                <div class="ibox-content">
+                    <form class="m-t" role="form" action="select">
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="Username" required="">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Password" required="">
+                        </div>
+                        <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
+
+                        <a href="#">
+                            <small>Forgot password?</small>
+                        </a>
+
+                        <!--p class="text-muted text-center">
+                            <small>Do not have an account?</small>
+                        </p>
+                        <a class="btn btn-sm btn-white btn-block" href="register">Create an account</a>
+                    </form>
+                    <p class="m-t">
+                        <small>Inspinia we app framework base on Bootstrap 3 &copy; 2020</small>
+                    </p>-->
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <div class="row">
+            <div class="col-md-6">
+                Digital Company
+            </div>
+            <div class="col-md-6 text-right">
+               <small>© 2020</small>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ select.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>HMS | Login 2</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+
+
+        <div style="max-width:1300px;" class="loginColumns animated fadeInDown">
+                <div class="row">
+        
+                    <div class="col-md-3">
+                        <h2 class="font-bold">Welcome to HMS</h2>
+        
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                        </p>
+                            <hr/>
+                        <p>
+                            Quick Links
+                        </p>
+        
+                        <p>
+                            <a href="">Link 1</a>
+                        </p>
+                        <p>
+                                <a href="">Link 2</a>
+                            </p>
+                            <p>
+                                    <a href="">Link 3</a>
+                                </p>
+                                                
+                    </div>
+                    <div class="col-md-9">
+                            <div class="wrapper wrapper-content animated fadeInUp">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <div class="contact-box">
+                                                <a href="link1">
+                                                <div class="col-sm-4">
+                                                    <div class="text-center">
+                                                            <h3><strong>ICAT</strong></h3>
+
+                                                        <img alt="image" class="img-circle m-t-xs img-responsive" src="img/a3.jpg">
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-8">
+                                                    <h3><strong>Institutional Capaity Assessment Tool</strong></h3>
+                                                </div>
+                                                <div class="clearfix"></div>
+                                                    </a>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <div class="contact-box">
+                                                <a href="link2">
+                                                        <div class="col-sm-4">
+                                                                <div class="text-center">
+                                                                        <h3><strong>SPAC</strong></h3>
+            
+                                                                    <img alt="image" class="img-circle m-t-xs img-responsive" src="img/a3.jpg">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-sm-8">
+                                                                <h3><strong>School Performance Score Card</strong></h3>
+                                                            </div>
+                                                            <div class="clearfix"></div>
+                                                    </a>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <div class="contact-box">
+                                                <a href="link3">
+                                                        <div class="col-sm-4">
+                                                                <div class="text-center">
+                                                                        <h3><strong>TMIS</strong></h3>
+            
+                                                                    <img alt="image" class="img-circle m-t-xs img-responsive" src="img/a3.jpg">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-sm-8">
+                                                                <h3><strong>Teacher Management Information System</strong></h3>
+                                                            </div>
+                                                            <div class="clearfix"></div>
+                                                    </a>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <div class="contact-box">
+                                                <a href="link4">
+                                                        <div class="col-sm-4">
+                                                                <div class="text-center">
+                                                                        <h3><strong>BPT</strong></h3>
+            
+                                                                    <img alt="image" class="img-circle m-t-xs img-responsive" src="img/a3.jpg">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-sm-8">
+                                                                <h3><strong>Budget & Planning Tool</strong></h3>
+                                                            </div>
+                                                            <div class="clearfix"></div>
+                                                    </a>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <div class="contact-box">
+                                                <a href="link5">
+                                                        <div class="col-sm-4">
+                                                                <div class="text-center">
+                                                                        <h3><strong>EMIS</strong></h3>
+            
+                                                                    <img alt="image" class="img-circle m-t-xs img-responsive" src="img/a3.jpg">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-sm-8">
+                                                                <h3><strong>Education Management Information System</strong></h3>
+                                                            </div>
+                                                            <div class="clearfix"></div>
+                                                    </a>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <div class="contact-box">
+                                                <a href="link6">
+                                                        <div class="col-sm-4">
+                                                                <div class="text-center">
+                                                                        <h3><strong>FMIS</strong></h3>
+            
+                                                                    <img alt="image" class="img-circle m-t-xs img-responsive" src="img/a3.jpg">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-sm-8">
+                                                                <h3><strong>Facilitators Management Information System</strong></h3>
+                                                            </div>
+                                                            <div class="clearfix"></div>
+                                                    </a>
+                                            </div>
+                                        </div>
+ 
+                                    </div>
+                                        </div>
+                            
+                    </div>
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col-md-6">
+                        Digital Company
+                    </div>
+                    <div class="col-md-6 text-right">
+                       <small>© 2020</small>
+                    </div>
+                </div>
+            </div>   
+
+
+    <!-- Mainly scripts -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="js/inspinia.js"></script>
+    <script src="js/plugins/pace/pace.min.js"></script>
+
+</body>
+
+
+</html>
